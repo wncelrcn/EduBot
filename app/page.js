98cdf817ee01894aeb5a0136bd17d63a4ff625cd
grgttdln <georgettedalen@gmail.com>
2024-08-08 20:59:17 +0800

    Frontend Colors Edited

--- app/page.js
+++ app/page.js
@@ -111,7 +111,49 @@ export default function Home() {
   const handleBotClick = (button) => {
     setChatBot(button);
     setSystemPrompt(systemPrompts[button]);
-    console.log(`${button} clicked`);
+  };
+
+  const getIcon = (text) => {
+    switch (text) {
+      case "Coding Bot":
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#8C1AF6"
+          >
+            <path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z" />
+          </svg>
+        );
+      case "Grammar Helper":
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#8C1AF6"
+          >
+            <path d="M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z" />
+          </svg>
+        );
+      case "Research Assistant":
+        return (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#8C1AF6"
+          >
+            <path d="M560-564v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-600q-38 0-73 9.5T560-564Zm0 220v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-380q-38 0-73 9t-67 27Zm0-110v-68q33-14 67.5-21t72.5-7q26 0 51 4t49 10v64q-24-9-48.5-13.5T700-490q-38 0-73 9.5T560-454ZM260-320q47 0 91.5 10.5T440-278v-394q-41-24-87-36t-93-12q-36 0-71.5 7T120-692v396q35-12 69.5-18t70.5-6Zm260 42q44-21 88.5-31.5T700-320q36 0 70.5 6t69.5 18v-396q-33-14-68.5-21t-71.5-7q-47 0-93 12t-87 36v394Zm-40 118q-48-38-104-59t-116-21q-42 0-82.5 11T100-198q-21 11-40.5-1T40-234v-482q0-11 5.5-21T62-752q46-24 96-36t102-12q58 0 113.5 15T480-740q51-30 106.5-45T700-800q52 0 102 12t96 36q11 5 16.5 15t5.5 21v482q0 23-19.5 35t-40.5 1q-37-20-77.5-31T700-240q-60 0-116 21t-104 59ZM280-494Z" />
+          </svg>
+        );
+      default:
+        return null;
+    }
   };
 
   return (
@@ -136,7 +178,8 @@ export default function Home() {
             {Object.keys(systemPrompts).map((text) => (
               <ListItem key={text} disablePadding>
                 <ListItemButton onClick={() => handleBotClick(text)}>
-                  <ListItemText primary={text} />
+                  {getIcon(text)}
+                  <ListItemText sx={{ mx: 2 }} primary={text} />
                 </ListItemButton>
               </ListItem>
             ))}
@@ -186,15 +229,17 @@ export default function Home() {
                 >
                   <Box
                     bgcolor={
-                      message.role === "assistant"
-                        ? "primary.main"
-                        : "secondary.main"
+                      message.role === "assistant" ? "#f2f2f2" : "#ded1eb"
                     }
-                    color="white"
+                    color={message.role === "assistant" ? "#000000" : "#5a189a"}
                     borderRadius={10}
                     p={2}
-                    maxWidth="80%"
-                    sx={{ wordWrap: "break-word" }}
+                    sx={{
+                      wordWrap: "break-word",
+                      maxWidth: "75%",
+                      mx: 1,
+                      px: 3,
+                    }}
                   >
                     <ReactMarkdown>{message.content}</ReactMarkdown>
                   </Box>
@@ -209,11 +254,29 @@ export default function Home() {
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
                 disabled={isLoading}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#5a189a",
+                    },
+                  },
+                  "& .MuiInputLabel-root": {
+                    "&.Mui-focused": {
+                      color: "#5a189a",
+                    },
+                  },
+                }}
               />
               <Button
                 variant="contained"
                 onClick={sendMessage}
                 disabled={isLoading}
+                sx={{
+                  backgroundColor: "#5a189a",
+                  "&:hover": {
+                    backgroundColor: "#7b2cbf",
+                  },
+                }}
               >
                 {isLoading ? (
                   <CircularProgress size={24} color="inherit" />
